adfdc808d52e6af9ae4127551da6498ac202e571
Hang_69 <rusanlimbu123@gmail.com>
2025-07-05 18:47:48 +0545

    new

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+// import React from "react";
 import { FaCss3, FaFigma, FaHtml5, FaJs, FaReact } from "react-icons/fa";
-import { FaGoogle } from "react-icons/fa";
-import { RiNetflixFill } from "@remixicon/react";
-import { FaAmazon } from "react-icons/fa";
+// import { FaGoogle } from "react-icons/fa";
+// import { RiNetflixFill } from "@remixicon/react";
+// import { FaAmazon } from "react-icons/fa";
+import logo from "../../assets/Bridge-Logo-o.png";
 
 export default function Experience() {
   return (
     <div id="Experience" className="p-10 md:p-24">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Exprience</h1>
-      <div className="flex flex-warp items-center justify-around">
-        <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 py-10">
+      <div className="flex md:flex-row flex-col items-center justify-between">
+        <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 md:pl-0 py-10">
           <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
             <FaHtml5 color="#E34F26" size={50} />
           </span>
@@ -27,41 +28,14 @@ export default function Experience() {
           </span>
         </div>
         <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
-          <FaGoogle color="#4285F4" size={50} />
+          {/* <FaGoogle color="#4285F4" size={50} /> */}
+          <img className="object-contain w-8 C-logo" src={logo} alt="" />
           <span className="text-white">
-            <h2 className="leading-tight">Software Engineer,Google</h2>
-            <p className="text-sm leading-tight font-thin">
-              Sept 2023 - Present
-            </p>
+            <h2 className="leading-tight">Web Developer, Bridge Software Solution</h2>
+            <p className="text-sm leading-tight font-thin">Sept 2023 - Present</p>
             <ul className="text-sm p-2">
-              <li>- Work as software developer</li>
-              <li>- Senior SDE Developer</li>
-            </ul>
-          </span>
-        </div>
-        <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
-          <RiNetflixFill color="#E50914" size={50} />
-          <span className="text-white">
-            <h2 className="leading-tight">Software Engineer,Google</h2>
-            <p className="text-sm leading-tight font-thin">
-              Sept 2023 - Present
-            </p>
-            <ul className="text-sm p-2">
-              <li>- Work as software developer</li>
-              <li>- Senior SDE Developer</li>
-            </ul>
-          </span>
-        </div>
-        <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
-          <FaAmazon color="#FF9900" size={50} />
-          <span className="text-white">
-            <h2 className="leading-tight">Software Engineer,Google</h2>
-            <p className="text-sm leading-tight font-thin">
-              Sept 2023 - Present
-            </p>
-            <ul className="text-sm p-2">
-              <li>- Work as software developer</li>
-              <li>- Senior SDE Developer</li>
+              <li>- Work as Webdeveloper developer</li>
+              <li>- Responsive SEO friendly Websites</li>
             </ul>
           </span>
         </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
       <div className="md:w-2/4 md:pt-10">
-        <h1 className="text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter">
+        <h1 className="text-3xl md:text-5xl font-bold flex leading-normal tracking-tighter">
           <TextChanger />
         </h1>
         <p className="text-sm md:text-2xl tracking-tight">
